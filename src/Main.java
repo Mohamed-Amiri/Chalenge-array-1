a90e15a7cb900848097ac27ea285b4daa5923f09
Mohamed-Amiri <mohamedamiri281@gmail.com>
2024-12-23 21:21:02 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Création d'un tableau pour stocker 5 nombres entiers
+        
         int[] numbers = new int[5];
 
-        // Lecture des 5 nombres entiers saisis par l'utilisateur
         System.out.println("Veuillez entrer 5 nombres entiers :");
         for (int i = 0; i < numbers.length; i++) {
             System.out.print("Nombre " + (i + 1) + ": ");
             numbers[i] = scanner.nextInt();
         }
 
-        // Affichage des nombres dans l'ordre inverse
         System.out.println("\nLes nombres dans l'ordre inverse sont :");
         for (int i = numbers.length - 1; i >= 0; i--) {
             System.out.println(numbers[i]);
         }
 
-        // Pas besoin de scanner.close(), car System.in reste ouvert pour d'autres saisies
+        
     }
 }
